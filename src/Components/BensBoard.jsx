d902c98881174254e432a29134b23449e0e5108a
benpapac <ben.papac@gmail.com>
2021-12-05 10:04:04 -0800

    add setActive, setTarget, and getCoordinates functions

--- src/Components/BensBoard.jsx
+++ src/Components/BensBoard.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
 import "../BensBoard.css";
+import { Movement } from '../Movement';
 
 function BensBoard(props) {
     const columns = ['A','B','C','D','E','F','G','H'];
     const rows = ['1','2','3','4','5','6','7','8'];
     const pieces = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'];
+    const currentPlayer =  'white';
+
+    const [active, setActive] = useState({
+        square: null,
+        piece: null,
+        coordinates: null
+    });
+    const [target, setTarget] = useState({
+        square: null,
+        piece: null,
+        coordinates: null
+    });
 
 
     const makePieces = (rowIndex, colIndex) => {
@@ -12,11 +25,78 @@ function BensBoard(props) {
         let color = rowIndex <= 1 ? 'white' : 'black';
         let thisPiece = (rowIndex === 0 || rowIndex === 7) ? pieces[colIndex]: 'pawn';
 
-        return <div className={`${color}-piece ${thisPiece}`}> {`${color} ${thisPiece}`} </div>;
+        return <div className={`${color}-${thisPiece}`}> {`${color} ${thisPiece}`} </div>;
+    }
+
+    const getData = async (e) => {
+        console.log('updating...', e.target.className);
+        if(e.target.className === 'square') {
+           return {
+            class: e.target.className,
+            color: null,
+            piece: null,
+            square: e.target.id
+            }
+        }
+       else return {
+            class: e.target.className,
+            color: e.target.className.substring(0,5),
+            piece: e.target.className.substring(6),
+            square: e.target.parentElement.id
+        };
+    }
+
+    const getCoordinates = (square) => {
+        if(!square) return;
+        let column = columns.indexOf(square.substring(0,1));
+        let row = rows.indexOf(square.substring(1));
+
+        return {colIndex: column, rowIndex: row};
+    }
+
+    const checkSquare = (data) => {
+        console.log('checking square...', 'class =', data.class);
+        if (data.class === 'square' && active.piece) {
+            setTarget({
+                piece: null,
+                square: data.square,
+                coordinates: getCoordinates(data.square),
+            });
+            return data.square;
+        }
+    }
+    const checkForPiece = (data) => {
+        console.log('checking color... color =', data.color);
+        if(data.class === 'square') return;
+        if(data.color === currentPlayer) {
+            console.log('setting active...');
+                setActive({
+                    piece: data.color.concat(data.piece),
+                    square: data.square,
+                    coordinates: getCoordinates(data.square),
+                });
+                return data;
+        }
+        else {
+            if(!active.piece) return;
+            console.log('setting target...');
+            setTarget({
+                    piece: data.color.concat(data.piece),
+                    square: data.square,
+                    coordinates: getCoordinates(data.square)
+                });
+                return data;
+        }
+    }
+
+    const updateSquares = async (e) => {
+        const data = await getData(e);
+        checkSquare(data);
+        checkForPiece(data);
     }
 
     return (
-        <section className='board'>
+        <section className='board' onClick={updateSquares}>
             {columns.map( (column) => {
                return rows.map( (row) => {
                     let colIndex = columns.indexOf(column);
--- src/Movement.jsx
+++ src/Movement.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { act } from 'react-dom/test-utils';
 
 
-export class Movement {
-    constructor(  targetRow, activeRow, targetColumn, activeColumn ){
-         this.slope = (targetRow - activeRow)/(targetColumn - activeColumn);
-         this.targetColumn = targetColumn;
-         this.targetRow = targetRow;
-         this.activeColumn = activeColumn;
-         this.activeRow = activeRow;
+/*export class Movement {
+    constructor(  targetSquare, activeSquare ){
+        this.targetColumn = targetSquare.substring(0,1);
+        this.targetRow = targetSquare.substring(1);
+        this.activeColumn = activeSquare.substring(0,1);
+        this.activeRow = activeSquare.substring(1);
+        this.slope = (this.targetRow - this.activeRow)/(this.targetColumn - this.activeColumn);
      }
 
     upOne () {
@@ -45,4 +45,37 @@ export class Movement {
     lShape () {
         return (Math.abs(this.slope) === 2 || Math.abs(this.slope) === 1/2);
     }
+    
+}*/
+
+export const Movement = {
+
+    king: (activeSquare, target) => {
+
+    },
+
+    queen: (activeSquare, target) => {
+
+    },
+
+    bishop: (activeSquare, target) => {
+
+    },
+
+    knight: (activeSquare, target) => {
+
+    },
+
+    rook: (activeSquare, target) => {
+
+    },
+
+    whitePawn: (activeSquare, target) => {
+
+    },
+
+    blackPawn: (activeSquare, target) => {
+
+    }
+
 }
